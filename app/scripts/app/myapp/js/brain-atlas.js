169c0dc1381d0461d96e63dcc0b507540d7dcb32
Mirco Nasuti <mirco.nasuti@chuv.ch>
2015-10-21 16:29:00 +0200

    First try

---
+++ app/scripts/app/myapp/js/brain-atlas.js
@@ -0,0 +1,45 @@
+// LUT to match DB structures with Allen Brain Atlas structures
+var structureLUT = new Array();
+structureLUT[4133]='A1C';
+structureLUT[4697]='CBC';
+structureLUT[4181]='V1C';
+structureLUT[4010]='M1C';
+structureLUT[4071]='M1C';
+structureLUT[4085]='S1C';
+structureLUT[4125]='S1C';
+// TODO: complete liste
+
+// Initialization of the brain selector
+function initBrainSelect() {
+	colorSelect = "green";
+
+	svgDoc = $("#svgObj")[0].contentDocument;
+	svgStructs = svgDoc.getElementsByClassName("svg_structure");  // DO NOT USE JQuery for that -> http://benfrain.com/selecting-svg-inside-tags-with-javascript/
+
+	selected = null;
+
+	for (var i = 0; i < svgStructs.length; i++) {
+		svgStructs[i].addEventListener('click', select, false);
+	}
+};
+
+// Select a structure
+function select(event) {
+	if (selected) {
+		for (var i = 0; i < svgStructs.length; i++) {
+			if(svgStructs[i].getAttribute('structure_id') == selected.attr('structure_id'))
+				{
+				svgStructs[i].style.fill = null
+				}
+		}
+	}
+	selected = $(event.currentTarget);
+	for (var i = 0; i < svgStructs.length; i++) {
+		if(svgStructs[i].getAttribute('structure_id') == selected.attr('structure_id'))
+			{
+			svgStructs[i].style.fill = colorSelect;
+			}
+	}
+	//alert("id = " + $(event.currentTarget).attr('structure_id'));
+	$("#editbox_search_structure_id").val(structureLUT[$(event.currentTarget).attr('structure_id')]);
+};
